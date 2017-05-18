eacbeabc8cbfa942cd91ef8f70f875d1f1733efa
Tom <tckerr@gmail.com>
2017-05-17 21:13:32 -0400

    Hacked json component to work

--- src/app/website/lobby/game-detail/game-actions/game-json/game-json.component.ts
+++ src/app/website/lobby/game-detail/game-actions/game-json/game-json.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnChanges, OnInit} from "@angular/core";
 import {GameSummary} from "../../../../../finalsweek-api/game/models/summary/game-summary";
+import {Observable} from "rxjs/Observable";
 
 @Component({
    selector:    "app-game-json",
@@ -9,13 +10,15 @@ import {GameSummary} from "../../../../../finalsweek-api/game/models/summary/gam
 
 export class GameJsonComponent implements OnInit, OnChanges {
    @Input() public gameSummary: GameSummary;
+   private loading = true;
 
-   ngOnInit(): void {
-   }
+   ngOnInit(): void {}
 
    ngOnChanges(changes: object) {
       try {
+         this.loading = true;
          this.gameSummary = changes["gameSummary"].currentValue;
+         Observable.timer(1).subscribe(() => this.loading = false);
       } catch (e) {
          console.error("Error updating game detail data.", e);
       }
